a17ffe817da17ede45933abf412b21a99fd0b592
Preetha2005 <127740734+Preetha2005@users.noreply.github.com>
2025-02-06 14:03:34 +0530

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@
     docker login -u preethasaravanan -p Preetha@2005
     docker tag test preethasaravanan/devops_day4
     docker push preethasaravanan/devops_day4
-    kubectl create deployment deploy5 --image=preethasaravanan/devops_day4 --port=80
-    kubectl expose deployment deploy5 --type=NodePort --port=80
     
